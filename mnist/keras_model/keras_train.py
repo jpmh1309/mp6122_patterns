a70fb0384d77a0f400dbdde6040cf3da0df54032
Jose Martinez <jpmh.1309@estudiantec.cr>
2022-05-01 10:32:09 -0600

    Mnist working 3

--- mnist/keras_model/keras_train.py
+++ mnist/keras_model/keras_train.py
@@ -1,10 +1,8 @@
 from keras import layers, models
-from keras.models import load_model
 from keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
-from keras import backend as K
-import tensorflow as tf
 
+# Preprocessing
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = x_train.astype('float32') / 255
@@ -16,6 +14,7 @@ x_test = x_test.reshape(-1, 28, 28, 1)
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
+# Create the model
 model = models.Sequential()
 model.add(layers.Conv2D(16, 3, activation='relu', input_shape=(28, 28, 1)))
 model.add(layers.MaxPool2D())
@@ -27,13 +26,22 @@ model.add(layers.Flatten())
 model.add(layers.Dense(256, activation='relu'))
 model.add(layers.Dropout(0.5))
 model.add(layers.Dense(10, activation='softmax'))
+
+# Print summary of the model
 model.summary()
 
+# Train the model
 model.compile(optimizer='adam', metrics=['accuracy'], loss='categorical_crossentropy')
 
-history = model.fit(x_train, y_train, epochs=5, batch_size=128)
+batch_size = 128
+epochs = 15
 
-print(model.evaluate(x_test, y_test))
+history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
+# Test the model
+score = model.evaluate(x_test, y_test, verbose=0)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
 
 # Save model and weights to separated files.
 with open("model.json", "w") as file:
